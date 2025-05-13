534cbd8d86c0866f0b7f077d716eab0b112ca4ef
Rotoros <gonzalo.poyatos@insbaixcamp.cat>
2025-05-13 20:21:24 +0200

    Afegir la funcio de prestarllibre i la de retornarLlibre

--- biblioteca.java
+++ biblioteca.java
@@ -1,18 +1,53 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class Biblioteca {
-private List<Llibre> llibres;
-public Biblioteca() {
-this.llibres = new ArrayList<>();
-}
-public void afegirLlibre(Llibre llibre) { llibres.add(llibre); }
-public Llibre buscarLlibre(String titol) {
-for (Llibre llibre : llibres) {
-if (llibre.getTitol().equalsIgnoreCase(titol)) {
-return llibre;
-}
-}
-return null;
-}
-public List<Llibre> getLlibres() { return llibres; }
+    private List<Llibre> llibres;
+
+    public Biblioteca() {
+        this.llibres = new ArrayList<>();
+    }
+
+    public void afegirLlibre(Llibre llibre) {
+        llibres.add(llibre);
+    }
+
+    public Llibre buscarLlibre(String titol) {
+        for (Llibre llibre : llibres) {
+            if (llibre.getTitol().equalsIgnoreCase(titol)) {
+                return llibre;
+            }
+        }
+        return null;
+    }
+
+    public List<Llibre> getLlibres() {
+        return llibres;
+    }
+
+    
+    public boolean prestarLlibre(Lector lector, String titol) {
+        Llibre llibre = buscarLlibre(titol);
+        if (llibre != null) {
+            lector.afegirLlibrePrestado(llibre);
+            llibres.remove(llibre);
+            return true;
+        }
+        return false; 
+    }
+
+    // Devolver un libro
+    public void retornarLlibre(Lector lector, String titol) {
+        Llibre llibre = null;
+        for (Llibre l : lector.getLlibresPrestats()) {
+            if (l.getTitol().equalsIgnoreCase(titol)) {
+                llibre = l;
+                break;
+            }
+        }
+        if (llibre != null) {
+            lector.getLlibresPrestats().remove(llibre);
+            llibres.add(llibre); 
+        }
+    }
 }
